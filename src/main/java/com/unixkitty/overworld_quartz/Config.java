ffe498cc3e9ba4d2b00c4bd7a86168f9c9fab62a
Unixkitty <uni@unixkitty.com>
2021-08-25 19:28:54 +0300

    Upped slightly default generation rate for the ore

--- src/main/java/com/unixkitty/overworld_quartz/Config.java
+++ src/main/java/com/unixkitty/overworld_quartz/Config.java
@@ -27,7 +27,7 @@ public class Config
 
         commonConfig.push("settings");
         quartzVeinSize = commonConfig.defineInRange("quartzVeinSize", 14, 1, OREGEN_THRESHOLD);
-        quartzVeinsPerChunk = commonConfig.defineInRange("quartzVeinsPerChunk", 10, 1, OREGEN_THRESHOLD);
+        quartzVeinsPerChunk = commonConfig.defineInRange("quartzVeinsPerChunk", 16, 1, OREGEN_THRESHOLD);
         quartzMinHeight = commonConfig.defineInRange("quartzMinHeight", 1, 1, WORLD_HEIGHT - 2);
         quartzMaxHeight = commonConfig.defineInRange("quartzMaxHeight", 64, 1, WORLD_HEIGHT - 1);
         commonConfig.pop();
